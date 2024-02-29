1de7b15fee1cd9f3fe2c9fdf7621b6bee2e6cec8
Valouuu94 <valentinroche94@gmail.com>
2024-02-29 09:27:21 +0100

    setup go

---
+++ url_shortner/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "monapp",
+		Usage: "Une application CLI simple",
+		Action: func(c *cli.Context) error {
+			fmt.Println("Bonjour, monde!")
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
